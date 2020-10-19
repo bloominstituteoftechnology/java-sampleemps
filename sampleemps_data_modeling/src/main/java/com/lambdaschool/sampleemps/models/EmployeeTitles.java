f63f148337235d007877323b51a138e8684df9d2
John Mitchell <jrmmba@outlook.com>
2020-10-19 16:23:04 -0700

    Updating Documentation

--- sampleemps_data_modeling/src/main/java/com/lambdaschool/sampleemps/models/EmployeeTitles.java
+++ sampleemps_data_modeling/src/main/java/com/lambdaschool/sampleemps/models/EmployeeTitles.java
@@ -2,12 +2,7 @@ package com.lambdaschool.sampleemps.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.io.Serializable;
 
 @Entity
@@ -20,6 +15,7 @@ import java.io.Serializable;
  * When you have a compound primary key, you must implement Serializable for Hibernate
  * When you implement Serializable you must implement equals and hash code
  */
+@IdClass(EmployeeTitlesId.class)
 public class EmployeeTitles extends Auditable implements Serializable
 {
     /*
